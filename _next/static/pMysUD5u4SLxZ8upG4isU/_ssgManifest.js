003ec2d400521c17a69a62ea1963a95a6cac8b35
dat.nguyen <dat.nguyen@transperfect.com>
2026-08-10 17:14:02 +0700

    Updates

---
+++ _next/static/pMysUD5u4SLxZ8upG4isU/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcatalog","\u002F[locale]\u002Finfo\u002F[slug]","\u002F[locale]\u002Fproduct\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
